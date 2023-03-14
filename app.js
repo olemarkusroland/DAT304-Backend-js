78646dff6c6e93234d529b7655f9042a8a79e2f5
Oskar Eftedal Markussen <oskar@uia.no>
2023-03-14 13:35:46 +0100

    add setInterval

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import  server  from './localAPI.js';
 //console.log(updateGlucose());
 //cosole.log(readLatestInsulin());
 //console.log(updateInsulin());
-
+setInterval(updateGlucose, 5 * 60 * 1000);
+setInterval(updateGlucose, 5 * 60 * 1000);
 console.log("hei")
 
 server();
