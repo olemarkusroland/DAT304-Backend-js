21be81f3afaac2df8317eb9368ab33513fa8c076
Oskar Eftedal Markussen <oskar@uia.no>
2023-03-14 13:07:39 +0100

    Add localAPI

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import { glucoseGET, insulinGET } from './nightscoutAPI.js'
 import { realmTest, readLatestGlucose, updateGlucose, readLatestInsulin, updateInsulin } from './realm.js'
+import  server  from './localAPI.js';
 
 //console.log(updateGlucose());
 //cosole.log(readLatestInsulin());
-console.log(updateInsulin());
+//console.log(updateInsulin());
+
+console.log("hei")
+
+server();
--- localAPI.js
+++ localAPI.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+export default async function server() {
+    app.listen(3000, () => {
+        console.log('Server listening on port 3000');
+    });
+};
